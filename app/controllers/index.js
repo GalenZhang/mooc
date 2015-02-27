ca34bf29840b045cdf535f2f3a61d64cf028bcf6
galen.zhang <z60014840@163.com>
2015-02-27 10:31:10 +0800

    Add category info, add douban data.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,18 @@
 var Movie = require('../models/movie');
+var Category  = require('../models/category');
 
 // index page
 exports.index = function(req, res) {
-	console.log('user in session:');
-	console.log(req.session.user);
-
-	Movie.fetch(function(err, movies) {
-		if (err) {
-			console.log(err);
-		}
-		// res.render 有2个参数：跳转的模板页，跳转需要的参数
-		res.render('index', {
-			title: 'imooc 首页',
-			movies: movies
+	Category.find({})
+		.populate({path: 'movies', options: {limit: 5}})
+		.exec(function(err, categories) {
+			if (err) {
+				console.log(err);
+			}
+			// res.render 有2个参数：跳转的模板页，跳转需要的参数
+			res.render('index', {
+				title: 'imooc 首页',
+				categories: categories
+			});
 		});
-	});
 }
